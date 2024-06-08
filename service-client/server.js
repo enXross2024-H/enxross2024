cecdeae0509c452517ccf5c2782bf2e12f5e1d5b
sueCalystegia00 <susan.basket0410@gmail.com>
2024-06-08 18:25:57 +0900

    update: To run the development mode with HTTPS

---
+++ service-client/server.js
@@ -0,0 +1,25 @@
+// server.js
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'certs/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'certs/server.crt')),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Server started on https://localhost:3000');
+  });
+});
